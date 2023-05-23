ea2421d500f3135cfb3bc4242f02407d232d77ad
rajitha <dvrajitha@gmail.com>
2023-05-23 23:08:10 +0200

    Add TenantContext and Interceptor
    Interceptor is using HttpTenantResolver to get tenant id from request header and set it to ThreadLocal

--- src/main/java/com/mutitenant/demo/tenant/TenantContext.java
+++ src/main/java/com/mutitenant/demo/tenant/TenantContext.java
@@ -1,4 +1,17 @@
 package com.mutitenant.demo.tenant;
 
 public class TenantContext {
+    private static final ThreadLocal<String> tenantId = new InheritableThreadLocal<>();
+
+    public static void setTenantId(String tenant) {
+        tenantId.set(tenant);
+    }
+
+    public static String getTenantId() {
+        return tenantId.get();
+    }
+
+    public static void clear() {
+        tenantId.remove();
+    }
 }
--- src/main/java/com/mutitenant/demo/web/TenantInterceptor.java
+++ src/main/java/com/mutitenant/demo/web/TenantInterceptor.java
@@ -1,6 +1,7 @@
 package com.mutitenant.demo.web;
 
 import com.mutitenant.demo.tenant.HttpTenantResolver;
+import com.mutitenant.demo.tenant.TenantContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -17,17 +18,17 @@ public class TenantInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        TenantContext.setTenantId(httpTenantResolver.resolveTenantId(request));
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        TenantContext.clear();
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        TenantContext.clear();
     }
 }
