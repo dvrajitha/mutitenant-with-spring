5c084ac3dfbbe0f977fdb14481285ccd57c3983a
rajitha <dvrajitha@gmail.com>
2023-05-31 13:49:21 +0200

    Add login and web config

--- src/main/java/com/mutitenant/demo/web/TenantInterceptor.java
+++ src/main/java/com/mutitenant/demo/web/TenantInterceptor.java
@@ -4,12 +4,15 @@ import com.mutitenant.demo.tenant.HttpTenantResolver;
 import com.mutitenant.demo.tenant.TenantContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class TenantInterceptor implements HandlerInterceptor {
+    private final static Logger LOGGER = LoggerFactory.getLogger(TenantInterceptor.class);
     private final HttpTenantResolver httpTenantResolver;
 
     public TenantInterceptor(HttpTenantResolver httpTenantResolver) {
@@ -18,17 +21,21 @@ public class TenantInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        TenantContext.setTenantId(httpTenantResolver.resolveTenantId(request));
+        String tenant = httpTenantResolver.resolveTenantId(request);
+        TenantContext.setTenantId(tenant);
+        LOGGER.info("===================tenantName:{}============================",tenant);
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        LOGGER.info("===============================================2");
         TenantContext.clear();
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        LOGGER.info("===============================================3");
         TenantContext.clear();
     }
 }
